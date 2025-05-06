ef8fc65edb51ab515b222243f2925aeaccaf14f5
hslater <hamish.slater@hallowelt.com>
2025-05-06 19:22:38 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: I7f0ae2d83a0af64ce69302c6769bd555a776c4f2
    (cherry picked from commit e74d4a77bbdeb6a1dcb0a5c5793a4eb47d1f9901)

--- resources/ui/page/OverviewPage.js
+++ resources/ui/page/OverviewPage.js
@@ -1,6 +1,6 @@
 bs.util.registerNamespace( 'bs.instanceStatus.ui' );
 
-bs.instanceStatus.ui.OverviewPage = function( name, config ) {
+bs.instanceStatus.ui.OverviewPage = function ( name, config ) {
 	bs.instanceStatus.ui.OverviewPage.parent.call( this, name, config );
 	this.info = mw.config.get( 'bsInstanceStatus_StatusData' );
 
@@ -17,25 +17,25 @@ bs.instanceStatus.ui.OverviewPage.prototype.setupOutlineItem = function () {
 };
 
 bs.instanceStatus.ui.OverviewPage.prototype.renderItems = function () {
-	var layouts = [];
-	for ( var key in this.info ) {
+	const layouts = [];
+	for ( const key in this.info ) {
 		if ( !this.info.hasOwnProperty( key ) ) {
 			continue;
 		}
-		var data = this.info[key];
-		var items = [];
+		const data = this.info[ key ];
+		const items = [];
 		if ( Object.prototype.hasOwnProperty.call( data, 'icon' ) ) {
 			items.push( new OO.ui.IconWidget( { icon: data.icon } ) );
 		}
 		items.push( new OO.ui.LabelWidget( { label: data.label } ) );
 
-		var layout = new OO.ui.HorizontalLayout( {
+		const layout = new OO.ui.HorizontalLayout( {
 			classes: [ 'bs-instance-status-overview-item' ],
 			items: items
 		} );
 
-		var value = $( '<div>' ).addClass( 'bs-instance-status-overview-item-value' ).html( data.value );
-		layout.$element.append( value );
+		const $value = $( '<div>' ).addClass( 'bs-instance-status-overview-item-value' ).html( data.value );
+		layout.$element.append( $value );
 
 		layouts.push( layout.$element );
 	}
