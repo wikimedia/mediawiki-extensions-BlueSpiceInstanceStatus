1bda6834a69300476adfb87e9519eb9606b056ef
Robert Vogel <vogel@hallowelt.com>
2026-06-12 11:30:46 +0200

    Allow for multiple CIDR ranges
    There are valid cases, where IPs from different networks need to access this endpoint.
    
    ERM48311
    [5.1.x]
    [ai assisted]
    
    Change-Id: I51206c739a413b5daaa8a0b01e8581b1f804291b

--- src/Rest/StatusCheckHandler.php
+++ src/Rest/StatusCheckHandler.php
@@ -30,10 +30,22 @@ class StatusCheckHandler extends SimpleHandler {
 	public function execute() {
 		$config = $this->configFactory->makeConfig( 'bsg' );
 		$clientIP = $this->getClientIP();
-		if (
-			!IPUtils::isValidRange( $config->get( 'InstanceStatusCheckAllowedIP' ) ) ||
-			!IPUtils::isInRange( $clientIP, $config->get( 'InstanceStatusCheckAllowedIP' ) )
-		) {
+		$ipRestrictionsLegacy = explode( ',', $config->get( 'InstanceStatusCheckAllowedIP' ) ?? '' );
+		$ipRestrictions = array_merge(
+			$ipRestrictionsLegacy,
+			$config->get( 'InstanceStatusCheckAllowedIPs' )
+		);
+		$isAllowed = false;
+		foreach ( $ipRestrictions as $ipRestriction ) {
+			if (
+				IPUtils::isValidRange( $ipRestriction ) &&
+				IPUtils::isInRange( $clientIP, $ipRestriction )
+			) {
+				$isAllowed = true;
+				break;
+			}
+		}
+		if ( !$isAllowed ) {
 			throw new HttpException( 'permissiondenied', 401 );
 		}
 		return $this->getResponseFactory()->createJson(
@@ -45,7 +57,7 @@ class StatusCheckHandler extends SimpleHandler {
 	 * @return string
 	 * @throws MWException
 	 */
-	private function getClientIP() {
+	protected function getClientIP() {
 		return RequestContext::getMain()->getRequest()->getIP();
 	}
 
